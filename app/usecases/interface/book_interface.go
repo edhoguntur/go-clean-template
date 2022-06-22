735251da2ab180dbe52aada004aa32d0f0907fab
edhoguntur <edho.guntur@gmail.com>
2022-06-22 11:09:33 +0700

    book interface

--- app/usecases/interface/book_interface.go
+++ app/usecases/interface/book_interface.go
@@ -1,9 +1,14 @@
 package usecases
 
+import (
+	"context"
+	"go-clean-template/app/domain/model"
+)
+
 type BookRepository interface {
-	Create()
-	Edit()
-	Delete()
-	FindByID()
-	FindAll()
+	Create(ctx context.Context, book model.Book) error
+	Edit(ctx context.Context, book model.Book) error
+	Delete(ctx context.Context, id int64) error
+	FindByID(ctx context.Context, id int64) (*model.Book, error)
+	FindAll(ctx context.Context) (*model.Books, error)
 }
